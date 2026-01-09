809fb597d007faa52449d59060717c4fdfbfa1cd
René Giovanni Borella <rgb@mapster.dk>
2026-01-09 10:11:36 +0000

    Add footer and fix background image in dashboard

--- dashboard/app/components/Footer/index.js
+++ dashboard/app/components/Footer/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledFooter = styled.div`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    text-align: center;
+    padding: 15px 0;
+    background-color: rgba(242, 242, 242, 0.9);
+    border-top: 1px solid #ddd;
+    z-index: 1000;
+    
+    a {
+        color: inherit;
+        text-decoration: underline;
+    }
+`;
 
 function Footer() {
-    return (<div></div>);
+    return (<StyledFooter>Powered by <a href='https://github.com/geopartner/mapconnect-geocloud2'>Mapconnect</a>, based on <a href='https://github.com/mapcentia/geocloud2'>GeoCloud2</a></StyledFooter>);
 }
 
 export default Footer;
