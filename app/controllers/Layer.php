16a1d33818187943e60ee5d92648fd2ab80fc369
René Giovanni Borella <rgb@mapster.dk>
2026-04-09 13:22:10 +0000

    DX: more array fixes

--- app/controllers/Layer.php
+++ app/controllers/Layer.php
@@ -65,8 +65,8 @@ class Layer extends Controller
     public function put_records(): array
     {
         $data = json_decode(urldecode(Input::get(null, true)), true);
-        if (!is_array($data["data"][0])) {
-            $data["data"] = [0 => $data["data"]];
+        if (!isset($data["data"]) || !is_array($data["data"][0] ?? null)) {
+            $data["data"] = [0 => $data["data"] ?? null];
         }
         foreach ($data["data"] as $datum) {
             $response = $this->auth($datum["_key_"]);
