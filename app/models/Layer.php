d445aed4ee8f5597d5dab92a38337f8a0129515a
René Giovanni Borella <rgb@mapster.dk>
2026-09-11 09:45:09 +0000

    Fix syntax

--- app/models/Layer.php
+++ app/models/Layer.php
@@ -135,33 +135,34 @@ class Layer extends Table
 
         // Case 2: We are looking for a column that only exists in the view
         $view_columns = ["coord_dimension", "srid", "type", "_key_"]; // _key_ is added to the list in order to check for relevancy.
+        $columnEsc = str_replace('"', '""', $column);
+
         if (in_array($column, $view_columns)) {
-            
+
             // Escape values by doubling single quotes (PostgreSQL string escape)
             $schemaEsc = str_replace("'", "''", $schema);
             $tableEsc = str_replace("'", "''", $table);
             $geomEsc = str_replace("'", "''", $geom);
             // Escape column identifier by doubling double quotes
-            $columnEsc = str_replace('"', '""', $column);
+            
             
             $sql = "SELECT \"$columnEsc\" FROM settings.getColumns('f_table_schema = ''$schemaEsc'' AND f_table_name = ''$tableEsc'' AND f_geometry_column = ''$geomEsc''', 'r_table_schema = ''$schemaEsc'' AND r_table_name = ''$tableEsc'' AND r_raster_column = ''$geomEsc''')";
             $res = $this->prepare($sql);
             $this->execute($res);
+            $row = $this->fetchRow($res);
+            return $row[$column] ?? null;
             
         } else {
 
             // Case 3: We are looking for columns that exist in the table, lets look in that instead. Only get the specific column requested.
-            $sql = "SELECT :column FROM settings.geometry_columns_join where _key_ = :key";
+            $sql = "SELECT \"$columnEsc\" FROM settings.geometry_columns_join where _key_ = :key";
             $res = $this->prepare($sql);
             $this->execute($res, [
-                ':column' => $column,
                 ':key' => $_key_,
             ]);
-
+            $row = $this->fetchRow($res);
+            return $row[$column] ?? null;
         }
-
-        $row = $this->fetchRow($res);
-        return $row[$column] ?? null;
     }
 
     /**
@@ -740,7 +741,6 @@ class Layer extends Table
             }
         }
         $arr = $filtered;
-
 
         foreach ($arr as $subuser) {
             $privileges->$subuser = $privileges->$subuser ?? "none";
